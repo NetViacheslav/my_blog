9e5f7df6252546d64f97c09f9bdc7658218be036
Netik Viacheslav <slava5582@gmail.com>
2023-07-29 15:12:13 +0200

    Adaptive view for SQL DB

--- blog/views.py
+++ blog/views.py
@@ -1,34 +1,21 @@
-from datetime import date
+from django.shortcuts import render, get_object_or_404
 
-from django.shortcuts import render
-
-all_posts = [
-    {
-        'title': "Programming Python",
-        'slug': "programming-python",
-        'image': "programming_goal.jpg",
-        'author': "Viacheslav",
-        'date': date(2023, 7, 24),
-        'excerpt': "Striving to learn and understand code will make you a programmer",
-        'content': "Some text about",
-    }
-]
-
-
-def get_date(post):
-    return post['date']
+from .models import Post
 
 
 def starting_page(request):
-    sorted_posts = sorted(all_posts, key=get_date)
-    latest_posts = sorted_posts[-3:]
+    latest_posts = Post.objects.all().order_by('-date')[:3]
     return render(request, 'blog/index.html', {'posts': latest_posts})
 
 
 def posts(request):
+    all_posts = Post.objects.all().order_by('-date')
     return render(request, 'blog/posts.html', {'all_posts': all_posts})
 
 
 def post_detail(request, slug):
-    identified_post = next(post for post in all_posts if post['slug'] == slug)
-    return render(request, 'blog/post_detail.html', {'post': identified_post})
+    identified_post = get_object_or_404(Post, slug=slug)
+    return render(request, 'blog/post_detail.html', {
+        'post': identified_post,
+        'post_tags': identified_post.tags.all()
+    })
