b761e324b56666f704b61a790448d497dfd59ccb
Netik Viacheslav <slava5582@gmail.com>
2023-08-08 22:02:13 +0200

    Change urls

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.starting_page, name='main-page'),
-    path('posts', views.posts, name='posts'),
-    path('posts/<slug:slug>', views.post_detail, name='post-detail'),
+    path('', views.StartingPageView.as_view(), name='main-page'),
+    path('posts', views.AllPostsView.as_view(), name='posts'),
+    path('posts/<slug:slug>', views.SinglePostView.as_view(), name='post-detail'),
 ]
