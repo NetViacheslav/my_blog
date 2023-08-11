a301d26bc86a5dec73907b5105c27cbf29508e7d
Netik Viacheslav <slava5582@gmail.com>
2023-08-11 15:35:21 +0200

    Add class Comment

--- blog/models.py
+++ blog/models.py
@@ -34,15 +34,9 @@ class Post(models.Model):
     author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name='posts')
     tags = models.ManyToManyField(Tag)
 
-    # def __str__(self):
-    #     return f'{self.title} by {self.author}'
-    #
-    # def get_absolute_url(self):
-    #     return reverse('post-detail', kwargs={'slug': self.slug})
-    #
-    # def save(self, *args, **kwargs):
-    #     self.slug = slugify(self.title)
-    #     super().save(*args, **kwargs)
-    #
-    # class Meta:
-    #     verbose_name_plural = 'posts'
+
+class Comment(models.Model):
+    user_name = models.CharField(max_length=120)
+    user_email = models.EmailField()
+    text = models.TextField(max_length=400)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
