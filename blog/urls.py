bee56361b6454065605aa785979f69a1407bbbcb
Netik Viacheslav <slava5582@gmail.com>
2023-08-13 15:12:11 +0200

    Add ReadLater url

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.StartingPageView.as_view(), name='main-page'),
     path('posts', views.AllPostsView.as_view(), name='posts'),
     path('posts/<slug:slug>', views.SinglePostView.as_view(), name='post-detail'),
+    path('read-later', views.ReadLater.as_view(), name='read-later')
 ]
